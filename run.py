62ab006f118d5cd4de07e9f4db61895a1a2ee889
KrisHoody <hoodykris@gmail.com>
2021-11-26 15:57:24 +0000

    Add comment to code

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
+"""
+ Import json library
+"""
 import json
 score = 0
 name = ""
+print("Welcome to movie quiz.")
 
+"""
+User can put only letter if enters his name.
+If name dont't contain letters user program stops and asks user for another try.
+"""
 while True:
     your_name = input("Enter your name, please: ")
     if your_name.isalpha():
-        print("Welcome to movie quiz," + your_name)   
+        print("Welcome," + your_name)   
         print("You will be presented with 20 Questions.")
         print("Enter your choice to get the points. Good luck!")
         break
@@ -13,6 +21,9 @@ while True:
          print("Only letters, please")
 
    
+"""
+Questions are printed without answers.
+"""
 
 def questions(all_questions):
     global score 
@@ -22,13 +33,27 @@ def questions(all_questions):
     print("3", all_questions["3"])
     print("4", all_questions["4"])
 
+    """
+    User answer.
+    """
+
     guest_answer = int(input('Your answer:\n'))
+
+    """
+    Ask user to put only number beetween 1 and four.
+    If number is smaller or greater is asked to choose beetween 1 and 4.
+    """
 
     while guest_answer > 4 or guest_answer <= 0:
         print("Please choose between 1 and 4")
         guest_answer = int(input("Please try again: "))
     else:
         return guest_answer
+
+    """
+    Checks if user answer is right, and if yes user earns a point.
+    If not right answer is displayed.
+    """
 
     if guest_answer == all_questions["right_answer"]:
         score += 1
@@ -41,12 +66,20 @@ def questions(all_questions):
 
 
 
-
+""" 
+Open json file with questions in another file.
+"""
 
 with open("quiz_main.json") as que_s:
     all_questions = json.load(que_s)
 
+    """
+    Loop to ask for question
+    """
+
     for i in range(0, len(all_questions)):
         questions(all_questions[i])
-
+"""
+Program prints number of points.
+"""
 print("This is the end of quiz. You've got", score, "points.")
